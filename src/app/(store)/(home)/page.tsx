d2a438f9773ef2f235e0e3bee74095a81496a061
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-03-10 09:53:31 -0300

    feat :sparkles: criação dos preços nas roupas

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
         href="/"
-        className="col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center itens-end"
+        className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center itens-end"
       >
         <Image
           src="/moletom-ai-side.png"
@@ -14,12 +14,19 @@ export default async function Home() {
           height={920}
           alt=""
           quality={100}
-          className=""
+          className="group-hover:scale-105 transition-transform duration-500"
         />
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moleton AI Side</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500  px-4 font-semibold">
+            R$129
+          </span>
+        </div>
       </Link>
+
       <Link
         href="/"
-        className="col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center itens-end"
+        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center itens-end"
       >
         <Image
           src="/moletom-java.png"
@@ -27,12 +34,19 @@ export default async function Home() {
           height={860}
           alt=""
           quality={100}
-          className=""
+          className="group-hover:scale-105 transition-transform duration-500"
         />
+        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moletom Java</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500  px-4 font-semibold">
+            R$129
+          </span>
+        </div>
       </Link>
+
       <Link
         href="/"
-        className="col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center itens-end"
+        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center itens-end"
       >
         <Image
           src="/camiseta-dowhile-2022.png"
@@ -40,8 +54,14 @@ export default async function Home() {
           height={860}
           alt=""
           quality={100}
-          className=""
+          className="group-hover:scale-105 transition-transform duration-500"
         />
+        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Camisa Dowhile</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500  px-4 font-semibold">
+            R$129
+          </span>
+        </div>
       </Link>
     </div>
   )
