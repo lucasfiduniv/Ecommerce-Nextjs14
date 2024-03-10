a655bcee886e31944ce40ed22074424314b09732
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-03-10 09:23:43 -0300

    feat :sparkles: criação  da pagina home com os grids das roupas

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,4 +1,48 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
-  return <p>initial commit</p>
+  return (
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href="/"
+        className="col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center itens-end"
+      >
+        <Image
+          src="/moletom-ai-side.png"
+          width={920}
+          height={920}
+          alt=""
+          quality={100}
+          className=""
+        />
+      </Link>
+      <Link
+        href="/"
+        className="col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center itens-end"
+      >
+        <Image
+          src="/moletom-java.png"
+          width={860}
+          height={860}
+          alt=""
+          quality={100}
+          className=""
+        />
+      </Link>
+      <Link
+        href="/"
+        className="col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center itens-end"
+      >
+        <Image
+          src="/camiseta-dowhile-2022.png"
+          width={860}
+          height={860}
+          alt=""
+          quality={100}
+          className=""
+        />
+      </Link>
+    </div>
+  )
 }
