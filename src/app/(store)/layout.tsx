29e687fd2f5f1ffe308027c2a7a90080aa63b94e
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-03-10 08:41:44 -0300

    feat :sparkles: criando loading home page e o header

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,5 +1,11 @@
+import { Header } from '@/components/header'
 import { ReactNode } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  )
 }
--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,3 +1,4 @@
-export default function Home() {
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
   return <p>initial commit</p>
 }
