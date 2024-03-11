d0a074451b41c61867c7d7a4bdab51775dbf02a7
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-03-11 13:39:24 -0300

    fix: :bug: arrumando para consumir do backend os produtos

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -11,16 +11,16 @@ async function getFeaturedProducts(): Promise<Product[]> {
 }
 
 export default async function Home() {
-  const products = await getFeaturedProducts();
-  console.log(products);
+  const [highlightedProduct, ...otherProducts] = await getFeaturedProducts();
+  console.log(highlightedProduct.image);
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href="/"
+        href={`/product/${highlightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center itens-end"
       >
         <Image
-          src="/moletom-ai-side.png"
+          src={highlightedProduct.image}
           width={920}
           height={920}
           alt=""
@@ -28,9 +28,9 @@ export default async function Home() {
           className="group-hover:scale-105 transition-transform duration-500"
         />
         <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moleton AI Side</span>
+          <span className="text-sm truncate">{highlightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500  px-4 font-semibold">
-            R$129
+            {highlightedProduct.price}
           </span>
         </div>
       </Link>
