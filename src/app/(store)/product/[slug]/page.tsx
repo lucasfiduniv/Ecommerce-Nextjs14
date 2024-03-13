7ae29a776bd93babcc7c9ed4d9010470caff3592
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2024-03-13 11:23:08 -0300

    fix: :bug: fazendo consumir da requesicao os dados do produto

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -24,7 +24,7 @@ export default async function ProductPage({ params }: ProductProps) {
     <div className="relative grid max-h-[860px] grid-cols-3">
       <div className="col-span-2 overflow-hidden">
         <Image
-          src="/moletom-never-stop-learning.png"
+          src={product.image}
           alt=""
           width={1000}
           height={1000}
@@ -32,18 +32,24 @@ export default async function ProductPage({ params }: ProductProps) {
         />
       </div>
       <div className="flex flex-col justify-center px-12">
-        <h1 className="text-3xl font-bold leading-tight">
-          Moletom Neber Stop Learning
-        </h1>
+        <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
         <p className="mt-2 leading-relaxed text-zinc-400">
-          Moletom fabricado com 88% de algodao e 12% de poliester.
+          {product.description}
         </p>
         <div className="mt-08 flex items-centergap-3">
           <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-            R$129
+            {product.price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
+            })}
           </span>
           <span className="text-sm text-zinc-400">
-            Em 12x s/ juros de R$10,75
+            {(product.price / 12).toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
           </span>
         </div>
         <div className="mt-8 space-y-4">
